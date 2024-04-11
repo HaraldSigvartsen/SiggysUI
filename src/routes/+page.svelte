25d6a1fe1fdc9e76e8cf0bfdeeda7ec9a227fa02
Harald Sigvartsen <harald.sigvartsen@noaignite.com>
2024-04-11 14:06:40 +0200

    feat: minor fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,16 +13,17 @@
 		<Button size="wide" variant="default" type="link" href="/docs" text="Get started"></Button>
 	</div>
 </div> -->
+<div class="z-0 overflow-y-hidden">
+	<Waves animationId={10}>
+		<div class="z-0 flex h-screen w-screen flex-col items-center justify-center">
+			<div class="flex h-1/4 flex-col items-center justify-center gap-y-4">
+				<h1 class="md:text-7xl">SiggysUI</h1>
+				<h2>An open source design system for building modern websites and applications.</h2>
+			</div>
 
-<Waves animationId={10}>
-	<div class="flex h-screen w-screen flex-col items-center justify-center">
-		<div class="flex h-1/4 flex-col items-center justify-center gap-y-4">
-			<h1 class="md:text-7xl">SiggysUI</h1>
-			<h2>An open source design system for building modern websites and applications.</h2>
+			<div class="h-1/4 items-center">
+				<Button size="wide" variant="default" type="link" href="/docs" text="Get started"></Button>
+			</div>
 		</div>
-
-		<div class="h-1/4 items-center">
-			<Button size="wide" variant="default" type="link" href="/docs" text="Get started"></Button>
-		</div>
-	</div>
-</Waves>
+	</Waves>
+</div>
